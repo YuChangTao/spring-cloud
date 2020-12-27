ed8e0b6b3849c581c7989dc66317eb1824e08fa9
yutao <2686222074@qq.com>
2020-12-27 15:10:58 +0800

    gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/yt/springcloud/config/GateWayConfig.java
@@ -0,0 +1,21 @@
+package com.yt.springcloud.config;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_rote_yt", r -> r.path("/guonei").uri("http://news.baidu.com")).build();
+        return routes.build();
+    }
+}
